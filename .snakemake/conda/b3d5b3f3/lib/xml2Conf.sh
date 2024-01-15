c0cf93082f0c3930593486261b4d81140bf03fc4
dralperenuysal <dralperenuysal@gmail.com>
2024-01-15 22:15:34 +0300

    interproscan plotting was done.

--- .snakemake/conda/b3d5b3f3/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/lib"
-XML2_LIBS="-lxml2 -L/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/lib -lz -L/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/lib -llzma  -L/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/lib -liconv -L/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/include/libxml2 -I/home/alperen_uysal/projects_ubuntu/alperen_miuul/.snakemake/conda/b3d5b3f3/include"
-MODULE_VERSION="xml2-2.9.14"
-
