83dc681225daf92c37afbc6f2be710b9dd56ca1c
dralperenuysal <dralperenuysal@gmail.com>
2024-01-28 23:52:48 +0300

    Interproscan data analysis was done.

--- scripts/ipr_concat.py
+++ scripts/ipr_concat.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import glob
+import os
 
-path = "resource/interproscan/*.tsv"
-out_file = "data/ipr_concat.csv"
+# Learn the current directory
+current_directory = os.getcwd()
+
+# Print the curent directory
+print("Current Working Directory:", current_directory)
+
+path = "../resource/interproscan/*.tsv"
+out_file = "../data/ipr_concat.csv"
 list_files = glob.glob(path)
 
 # Get the species names from the list_files
@@ -14,13 +21,13 @@ for element in list_files:
 
 dic_ann = {}
 for key, values in sp_dic.items():
-    dic_ann[key] = pd.read_csv(value, sep = "\t", header = None, names= list(range(0, 15)), engine= 'python', quoting = 3)[[0, 11, 12]][:100]
+    dic_ann[key] = pd.read_csv(values, sep = "\t", header = None, names= list(range(0, 15)), engine= 'python', quoting = 3)[[0, 11, 12]][:100]
     dic_ann[key] = dic_ann[key].dropna().drop_duplicates().rename(columns = {0: "id", 11: 'ipr', 12: 'ann_inter'})
 
     # add column with a species name
     dic_ann[key]['sp'] = key
-    dic_ann[key].to_csv(f"data/{key}.csv", sep = '\t', index = False)
+    dic_ann[key].to_csv(f"../data/{key}.csv", sep = '\t', index = False)
 
 # Concat all species
-concat = pd.concat(dic_ann, axis=0).dopna().drop_duplicates()
+concat = pd.concat(dic_ann, axis=0).dropna().drop_duplicates()
 concat.to_csv(out_file, sep = '\t', index = False)
